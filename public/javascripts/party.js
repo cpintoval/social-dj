b8273c97b7ce6e2490a4a5c934c214b00608d5d6
MrDhillon <harman.singh.dhillon@gmail.com>
2015-10-12 22:16:02 -0400

    api calls for song names

--- public/javascripts/party.js
+++ public/javascripts/party.js
@@ -8,9 +8,33 @@ $(function() {
     return false;
   });
 
-  socket.on('new song', function(song) {
-    $('#songs').append($('<li>').text(song));
-    console.log(song);
+  $('form').on('keypress','#song',function(song){
+    var $song_input =$('#song').val();
+    if ($song_input.length > 4){
+      $.ajax({
+        url: 'http://ws.spotify.com/search/1/track.json?q='+$song_input
+        }).success(function(response){
+          console.log($song_input);
+        autocomplete(response);
+      });
+    }
   });
 
+  socket.on('new song', function(song) {
+    $('#songs').append($('<li>').text(song));
+  });
+
+  function autocomplete(data){
+    var $matching_songs = $('#matching-songs');
+    $matching_songs.empty();
+    var tracks = data.tracks;
+    for(var i=0;i<5;i++){
+        var track = tracks[i];
+        var result = '<option id="' + track.name + '">' + track.name + ' by '+track.artists[0].name +'</option>';
+        $matching_songs.append(result);
+    }
+  }
+
+
 });
+
