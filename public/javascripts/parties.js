94dd947b831874dcb6591c9e05dee1500893d0c7
Tony Lei <tonykclei@gmail.com>
2015-10-15 01:31:59 +0000

    able to create new parties. separated new parties with old. refactored index.ejs

---
+++ public/javascripts/parties.js
@@ -0,0 +1,21 @@
+var newName;
+
+$('#newparty-button').click(function(event){
+  event.preventDefault();
+  newName = $('#new-party').val();
+
+  if(newName !== ''){
+
+    $.post('http://localhost:3000/parties', {partyName: newName}, function(response){
+      console.log(response);
+      if (response){
+        console.log(response);
+        $('#new-party').val('');
+        $('#created-parties').append('<li><a href="/parties/' + response.id + '">' + response.name + '</a></li>');
+
+      }
+    })
+  } else {
+    alert('Party name cannot be empty!');
+  }
+})
