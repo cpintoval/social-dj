7a1a9e745b3294e1a665a0e0019036f6c4549337
MrDhillon <harman.singh.dhillon@gmail.com>
2015-10-19 20:50:46 -0400

    spaced out cotes

--- public/javascripts/party.js
+++ public/javascripts/party.js
@@ -165,7 +165,7 @@ myApp.buildWeatherHTML = function () {
                   <div class='country'>"+song.artist+"\
                   </div>\
                 </div>\
-                <div class='item-after'><span id='upvoted'>"+song.voteCount+"&nbsp;</span><i class='fa fa-thumbs-up' id='upvote' ></i></div>\
+                <div class='item-after'><span id='upvoted'>"+song.voteCount+"</span>&nbsp;<i class='fa fa-thumbs-up' id='upvote' ></i></div>\
               </div>\
               </a>\
             </div>\
