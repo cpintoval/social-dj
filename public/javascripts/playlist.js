f4296ac7fd310ff25285b6ef9d928c745f13b741
MrDhillon <harman.singh.dhillon@gmail.com>
2015-10-20 21:12:22 -0400

    playlist page styling and ordering

--- public/javascripts/playlist.js
+++ public/javascripts/playlist.js
@@ -1,3 +1,18 @@
-$(function(){
-  console.log(songs);
+$(function() {
+  var $songsTable = $('tbody');
+  var $songsArray = $songsTable.children('tr');
+  $songsArray.sort(function(a, b) {
+    var votesA = a.getAttribute('data-vote');
+    var votesB = b.getAttribute('data-vote');
+    if (votesA > votesB) {
+      return -1;
+    }
+    else if (votesA < votesB) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  });
+  $songsArray.detach().appendTo($songsTable).fadeIn();
 });
